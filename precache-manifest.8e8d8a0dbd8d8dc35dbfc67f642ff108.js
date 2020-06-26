6c49253caf8be88ab0bada09117480f26b34d822
Nirmalie <wnirmalie@yahoo.com>
2020-06-26 23:23:03 +0200

    Updates

---
+++ precache-manifest.8e8d8a0dbd8d8dc35dbfc67f642ff108.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2d61ec16197073e5f69d5e9bb915d2",
+    "url": "/firstapp/index.html"
+  },
+  {
+    "revision": "7ba7b5418bf1b8de3d5a",
+    "url": "/firstapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6716138aa2aaa15f17eb",
+    "url": "/firstapp/static/css/main.0929fcbb.chunk.css"
+  },
+  {
+    "revision": "7ba7b5418bf1b8de3d5a",
+    "url": "/firstapp/static/js/2.2dadf5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstapp/static/js/2.2dadf5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6716138aa2aaa15f17eb",
+    "url": "/firstapp/static/js/main.95423837.chunk.js"
+  },
+  {
+    "revision": "dbc55e65f8c4164ac0a4",
+    "url": "/firstapp/static/js/runtime-main.cb7cf24a.js"
+  }
+]);
